14526bd00b440ccd65c4ffe2929166ba2682670b
misha <mihail_160505@mail.ru>
2024-03-25 19:04:06 +0300

    by name

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,10 @@
 #include<iostream>
+#include<string>
 using namespace std;
 int main(){
-
-cout<<"hello world!"<<endl;
-
+string name;
+cin>>name;
+cout<<"hello world from"<<name<<endl;
     return 0;
 }
 
