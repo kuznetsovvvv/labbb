49316c5923b3fcaeb9102e1dc89b39c873040775
misha <mihail_160505@mail.ru>
2024-03-25 19:16:47 +0300

    Removed using namespace std;

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,13 +1,13 @@
-#include<iostream>
-#include<string>
-using namespace std;
-int main(){
-string name;
-cin>>name;
-cout<<"hello world from"<<name<<endl;
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string name;
+    std::cout << "Enter your name: ";
+    std::cin >> name;
+
+    std::cout << "Hello world from " << name << std::endl;
     return 0;
 }
-
-
 
 
